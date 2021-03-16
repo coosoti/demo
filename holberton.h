2b01b359fce208bfc1e4c92c65762e3108f03722
coosoti <charlesosoti@gmail.com>
2021-03-16 11:46:00 +0300

    second attempt printf

---
+++ holberton.h
@@ -0,0 +1,29 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdarg.h>
+#include<stdlib.h>
+
+/**
+ * struct print - struct containing char for comparison with specifiers and
+ * then select appropriate function
+ * @type: char to compare
+ * @funct: function to print
+ *
+ */
+
+typedef struct print
+{
+	char type;
+	int (*funct)(va_list);
+
+} print_f;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int printNaN(char c1, char c2);
+int printchar(va_list a_list);
+int printstr(va_list a_list);
+int printint(va_list a_list);
+
+#endif
